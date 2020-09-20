ea178b052cb6b5265f3e7c270ba5287c58bdd6a3
Zeyu Wu <48381328+Uthergogogo@users.noreply.github.com>
2020-09-19 23:50:29 -0400

    Update 15.py

--- 15_3Sum/15.py
+++ 15_3Sum/15.py
@@ -2,26 +2,30 @@ def threeSum(nums):
     result = []
     nums.sort()  # Sorting the array takes O(nlog n)
     for i in range(len(nums)-2):
-        if nums[i] + nums[i+1] + nums[i+2] > 0:
+        if nums[i] + nums[i+1] + nums[i+2] > 0:  # 减少不必要的计算 可提速
             break
         if nums[i] + nums[-1] + nums[-2] < 0:
             continue
-        if i > 0 and nums[i - 1] == nums[i]:
+        if i > 0 and nums[i - 1] == nums[i]:  # 防止重复
             continue
         left, right = i + 1, len(nums) - 1
         while left < right:
-            if nums[i] + nums[left] + nums[right] == 0:
+            # 要用temp取代nums[i] + nums[left] + nums[right] 因为如果不用temp则每次都要进行运算 会增加运行时间
+            temp = nums[i] + nums[left] + nums[right]
+            if temp == 0:
                 result.append([nums[i], nums[left], nums[right]])
+                # 防止如[-2, 0, 0, 2, 2]等情况会出现两个[-2, 0, 2]的结果
                 while left < right and nums[left] == nums[left+1]:
                     left += 1
                 left += 1
+                # 防止如[-2, 0, 0, 2, 2]等情况会出现两个[-2, 0, 2]的结果
                 while right > 0 and nums[right] == nums[right-1]:
                     right -= 1
                 right -= 1
-
-            elif nums[i] + nums[left] + nums[right] < 0:
+            # 双指针正常操作
+            elif temp < 0:
                 left += 1
-            elif nums[i] + nums[left] + nums[right] > 0:
+            elif temp > 0:
                 right -= 1
     return result
 
