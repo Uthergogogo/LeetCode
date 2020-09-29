437753f594a40500b33cf42bb5ba69e2c7559adc
Zeyu Wu <48381328+Uthergogogo@users.noreply.github.com>
2020-09-28 23:42:12 -0400

    Update 33.py

--- 33_Search in Rotated Sorted Array/33.py
+++ 33_Search in Rotated Sorted Array/33.py
@@ -1,5 +1,29 @@
+# O(n)
 def search(nums, target):
     if target in nums:
         return nums.index(target)
     else:
         return -1
+
+
+# O(log n)
+def search2(nums, target):
+    # first need to find where is the rotation by using Binary Search
+    left, right = 0, len(nums)-1
+    while left <= right:
+        mid = (left + right)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] >= nums[left]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+
+print(search2([5, 1, 3], 3))
