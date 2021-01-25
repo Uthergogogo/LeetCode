d3daf55b8c2e65703dfe6da8ae2dd7e88429f472
Zeyu Wu <48381328+Uthergogogo@users.noreply.github.com>
2021-01-25 16:42:18 -0500

    Update 63.py

--- 63_Unique Paths II/63.py
+++ 63_Unique Paths II/63.py
@@ -23,7 +23,44 @@ def uniquePathsWithObstacles(obstacleGrid):
                 obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
             else:
                 obstacleGrid[i][j] = 0
+
     return obstacleGrid[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
 
+def uniquePathsWithObstacles2(obstacleGrid):
+    if obstacleGrid[0][0] == 1:
+        return 0
+    if obstacleGrid[0][0] == 0 and len(obstacleGrid) == 1 and len(obstacleGrid[0]) == 1:
+        return 1
+    obstacleGrid[0][0] = 1
+    for i in range(1, len(obstacleGrid[0])):
+        if obstacleGrid[0][i] == 0:
+            obstacleGrid[0][i] = obstacleGrid[0][i - 1]
+        else:
+            obstacleGrid[0][i] = 0
+            continue
+    for i in range(1, len(obstacleGrid)):
+        if obstacleGrid[i][0] == 0:
+            obstacleGrid[i][0] = obstacleGrid[i - 1][0]
+        else:
+            obstacleGrid[i][0] = 0
+            continue
 
-print(uniquePathsWithObstacles([[0, 0, 1, 0], [1, 0, 1, 0], [1, 0, 0, 0]]))
+    for i in range(1, len(obstacleGrid)):
+        for j in range(1, len(obstacleGrid[0])):
+            if obstacleGrid[i][j] == 0:
+                obstacleGrid[i][j] = 1
+            else:
+                obstacleGrid[i][j] = 0
+    for i in range(1, len(obstacleGrid)):
+        for j in range(1, len(obstacleGrid[0])):
+            if obstacleGrid[i][j] == 0:
+                continue
+            obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
+    return obstacleGrid[len(obstacleGrid) - 1][len(obstacleGrid[0]) - 1]
+
+
+# print(uniquePathsWithObstacles([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0],[0,1],[0,0],[0,0],[1,0],[0,0],[0,0],[0,1],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,1],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0]]))
+# print(uniquePathsWithObstacles2([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0],[0,1],[0,0],[0,0],[1,0],[0,0],[0,0],[0,1],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,1],[0,0],[0,0],[0,0],[0,0],[1,0],[0,0],[0,0],[0,0],[0,0]]))
+print(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+print(uniquePathsWithObstacles2([[0,0,0],[0,1,0],[0,0,0]]))
+# print(uniquePathsWithObstacles2([[0, 1, 0, 1, 0]]))
